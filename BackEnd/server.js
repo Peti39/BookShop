7b153685896f02737d3884a79fc422cc356acc9a
Petrik Diak <diak@petrik.hu>
2025-04-14 15:44:50 +0200

    Setup a basic json on the server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,18 @@
+//import data from './src/testData.json' assert { type: 'json' };
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const testData = {
+    title: "TestBook01",
+    price: 10
+}
+
+const data = require('./src/testData.json')
+
 app.get('/data', (req, res) => {
-    res.send('<h1>Hello, Geeks!</h1><p>This is your simple Express server.</p>');
+    res.json(data);
 });
 
 app.listen(PORT, () => {
