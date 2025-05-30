d167e2b15204ab3a27bd527f98dd2c2ac0fd0769
Peti39 <szpeti2006@gmail.com>
2025-05-30 04:01:08 +0200

    Made the frontend look better

--- FrontEndTest/BookStore/src/main.js
+++ FrontEndTest/BookStore/src/main.js
@@ -43,7 +43,61 @@ const fetchData = async () => {
   }
 };
 
+/*async function onLoad() {
+  const data = await fetchData();
+  data.books.forEach((book) => {
+    document.getElementById("book-list").innerHTML += `
+    <div class="row g-4">
+      <div class="col-md-4">
+        <div class="card h-100">
+          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="${book.title}">
+          <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">${book.author}</p>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-primary w-100" style="background-color: #6f4d38;">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    `
+  });
+}*/
 async function onLoad() {
+  const data = await fetchData();
+  const bookList = document.getElementById("book-list");
+  bookList.innerHTML = "";
+
+  if (!data || !data.books) return;
+
+  for (let i = 0; i < data.books.length; i += 3) {
+    const rowDiv = document.createElement("div");
+    rowDiv.className = "row g-4";
+
+    for (let j = i; j < i + 3 && j < data.books.length; j++) {
+      const book = data.books[j];
+      rowDiv.innerHTML += `
+        <div class="col-md-4">
+          <div class="card h-100">
+            <img src="${book.picture}" class="card-img-top" alt="${book.title}" style="width: 100%; height: 500px;">
+            <div class="card-body">
+              <h5 class="card-title">${book.title}</h5>
+              <p class="card-text">${book.author}</p>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary w-100" style="background-color: #6f4d38;">${book.price} Ft</button>
+            </div>
+          </div>
+        </div>
+      `;
+    }
+
+    bookList.appendChild(rowDiv);
+  }
+}
+
+onLoad();
+/*async function onLoad() {
   const data = await fetchData();
   if (data && data.books) {
     let table = document.createElement("table");
@@ -70,7 +124,7 @@ async function onLoad() {
 
     document.getElementById("app").appendChild(table);
   }
-}
+}*/
 
-onLoad();
+//onLoad();
 
